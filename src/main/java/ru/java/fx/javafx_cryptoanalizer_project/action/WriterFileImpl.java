702aca295acc99ad74cc4eaff567db3475e6a5a3
Maksim Gogolin <m4xin4t4@gmail.com>
2024-04-03 18:46:57 +0500

    В классе WriterFileImpl в 13 строке заменен аргумент на Paths.get(dst)

--- src/main/java/ru/java/fx/javafx_cryptoanalizer_project/action/WriterFileImpl.java
+++ src/main/java/ru/java/fx/javafx_cryptoanalizer_project/action/WriterFileImpl.java
@@ -3,13 +3,13 @@ package ru.java.fx.javafx_cryptoanalizer_project.action;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
-import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 public class WriterFileImpl implements WriterFile{
     public void writeFile(String dst, List<String> allLines){
         try{
-            Files.write(Path.of(dst), allLines, Charset.forName("windows-1251"));
+            Files.write(Paths.get(dst), allLines, Charset.forName("windows-1251"));
         }
         catch (IOException ex) {
             throw new RuntimeException("Ошибка ввода-вывода " + ex);
